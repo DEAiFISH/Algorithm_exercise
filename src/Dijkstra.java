9a7e675e95f5cf20184a40adce87c55bf77251f2
DEAIFISH <1277676309@qq.com>
2023-05-20 22:08:56 +0800

    -

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -39,8 +39,9 @@ public class Dijkstra {
             for (int j = 1; j < adjMatrix.length; j++) {  //然后，将顶点0到其它顶点的距离与加入中间顶点k之后的距离进行比较，更新最短距离
                 if (!used[j]) {  //当顶点j未被遍历时
                     //首先，顶点k到顶点j要能通行；这时，当顶点0到顶点j的距离大于顶点0到k再到j的距离或者顶点0无法直接到达顶点j时，更新顶点0到顶点j的最短距离
-                    if (adjMatrix[k][j] != -1 && (result[j] > min + adjMatrix[k][j] || result[j] == -1))
+                    if (adjMatrix[k][j] != -1 && (result[j] > min + adjMatrix[k][j] || result[j] == -1)) {
                         result[j] = min + adjMatrix[k][j];
+                    }
                 }
             }
         }
